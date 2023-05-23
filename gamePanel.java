c77fc8b23575d91af1d804de42f102ef349ab74b
Ilycster <i@hoshka.com>
2023-05-23 11:58:17 -0700

    Images

--- gamePanel.java
+++ gamePanel.java
@@ -6,12 +6,9 @@ import javax.swing.*;
 
 public class gamePanel extends JPanel {
 
-    static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
-    static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
-    static final int BALL_DIAMETER = 20;
-    static final int PADEL_WIDTH = 25;
-    static final int PADEL_HEIGHT = 100;
+   
 
+
+    
 
 }
