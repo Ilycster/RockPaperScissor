424a144bf32ea52c3caa10703e760ad19d2f25d5
Ilycster <i@hoshka.com>
2023-05-31 10:46:42 -0700

    window

--- Window.java
+++ Window.java
@@ -2,8 +2,6 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 
 public class Window implements ActionListener{
     JFrame frame = new JFrame();
@@ -12,31 +10,19 @@ public class Window implements ActionListener{
     Button Paper;
     Button Scissors;
 
-    public Window(String title, int x, int y, int width, int height, boolean fullscreen){
+    public Window(String title, int x, int y, int x1, int y1, boolean fullscreen){
 
-        frame = new JFrame(title);
-        frame.setPreferredSize(new Dimension(width, height));
         Rock = new Button(this, "Rock", 0, 360, 133, 40);
         Paper = new Button(this, "Paper", 133, 360, 133, 40);
         Scissors = new Button(this, "Scissors", 266, 360, 133, 40);
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int centerX = (screenSize.width - frame.getWidth()) / 2;
-        int centerY = (screenSize.height - frame.getHeight()) / 2;
-        frame.setLocation(centerX, centerY);
-
-        if (fullscreen) {
-            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            frame.setUndecorated(true);
-        } else {
-            frame.setBounds(x, y, width, height);
-        }
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(x1, y1);
+        frame.setLocation(x, y);
+        frame.setLayout(null);
         frame.setVisible(true);
-        
     }
-    
     public JFrame getFrame(){
         return frame;
     }
